e58ad0a1c9806c7019e489676fd98d358b6b8aca
Bandapalle <sourabh.v.bandapalle@accenture.com>
2025-12-12 23:57:14 +0530

    Initial Commit

--- project/rds/outputs.tf
+++ project/rds/outputs.tf
@@ -2,7 +2,8 @@ output "db_endpoint" {
   value = aws_db_instance.mysql.address
 }
 
+
 output "rds_secret_arn" {
-  value       = aws_secretsmanager_secret.rds.arn
-  description = "Secrets Manager ARN for RDS credentials"
+  description = "ARN of existing Secrets Manager secret 'demo'"
+  value       = data.aws_secretsmanager_secret.rds_existing.arn
 }
